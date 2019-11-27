14a0ca7e8a1015442d0e02de3f4a0d64f90431a5
Kieran Sherman <16752746+ShermanZero@users.noreply.github.com>
2019-11-27 04:39:42 -0500

    gitignore updates

--- build/resources/blacklist.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    words: ["nigger", "childfucker", "motherfucker", "nigga"]
-};
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYmxhY2tsaXN0LmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vc3JjL3Jlc291cmNlcy9ibGFja2xpc3QudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxrQkFBZTtJQUNiLEtBQUssRUFBRSxDQUFDLFFBQVEsRUFBRSxhQUFhLEVBQUUsY0FBYyxFQUFFLE9BQU8sQ0FBQztDQUMxRCxDQUFDIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQge1xuICB3b3JkczogW1wibmlnZ2VyXCIsIFwiY2hpbGRmdWNrZXJcIiwgXCJtb3RoZXJmdWNrZXJcIiwgXCJuaWdnYVwiXVxufTtcbiJdfQ==
